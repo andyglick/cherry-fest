488764165a2e2644fe6f010d95aa4e7e7c2498b9
mamalisk <mamalis.kostas@gmail.com>
2016-05-04 14:28:39 +0100

    changes

--- src/main/java/io/magentys/fest/MemoryKeys.java
+++ src/main/java/io/magentys/fest/MemoryKeys.java
@@ -2,11 +2,6 @@ package io.magentys.fest;
 
 public class MemoryKeys {
 
-    public static class Credentials {
-        public static final String USERNAME = "username";
-        public static final String PASSWORD = "password";
-    }
-
     public static class Screens {
         public static final String MAIN_WINDOW = "mainWindow";
     }
--- src/main/java/io/magentys/fest/missions/FindElementAndInvokeAction.java
+++ src/main/java/io/magentys/fest/missions/FindElementAndInvokeAction.java
@@ -1,6 +1,7 @@
 package io.magentys.fest.missions;
 
 import io.magentys.Agent;
+import io.magentys.fest.MemoryKeys;
 import io.magentys.fest.screens.SwingScreenElement;
 import org.fest.swing.core.ComponentFoundCondition;
 import org.fest.swing.fixture.FrameFixture;
@@ -25,7 +26,7 @@ public class FindElementAndInvokeAction extends FindElement {
     public Agent accomplishAs(Agent agent) {
         Pause.pause(
                 new ComponentFoundCondition(waitMessage(swingScreenElement),
-                        agent.recalls("mainWindow", FrameFixture.class).robot.finder(),
+                        agent.recalls(MemoryKeys.Screens.MAIN_WINDOW, FrameFixture.class).robot.finder(),
                         matcherFor(swingScreenElement, methodToInvoke, arguments)), 60000);
         return agent;
     }
